686601864aa955d2f4436181a39ce8af4c2d9ddd
asfaca <asfaca@naver.com>
2017-10-17 09:50:25 +0900

    add time function

--- test_module.c
+++ test_module.c
@@ -3,6 +3,7 @@
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/proc_fs.h>
+#include <linux/time.h>
 
 #define QUESIZE 100
 
@@ -33,13 +34,16 @@ struct file_operations myproc_fops = { .owner = THIS_MODULE,
 				       .open = my_open, 
 				       .write = my_write,
 };
+struct timespec my_bio_time;
+EXPORT_SYMBOL(my_bio_time);
 
 static int __init init_my_module(void) {
 	my_proc_dir = proc_mkdir("oslab_dir", NULL);
 	my_proc_file = proc_create("oslab_file", 0600, my_proc_dir, &myproc_fops);
 	
-
 	printk(KERN_ALERT "module starts.\n");
+	getnstimeofday(&my_bio_time);
+	printk("sec time = %ld, nc sec time = %ld\n", my_bio_time.tv_sec , my_bio_time.tv_nsec);
 	return 0;
 }
 
