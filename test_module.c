08e8406d831cb8f596f63a2f45098d4466643d8f
asfaca <asfaca@naver.com>
2017-10-17 11:05:13 +0900

    add que

--- test_module.c
+++ test_module.c
@@ -8,10 +8,17 @@
 #define QUESIZE 100
 
 /*declearation for this module*/
-struct io_desc {
+struct myio_desc {
 	struct timespec time;
 	sector_t sector_num;
 };
+
+struct myio_cir_que {
+	struct myio_desc que[QUESIZE];
+	int que_count;
+	int fir_index;
+	int curr_index;
+}
 
 static int my_open(struct inode *inode, struct file *file) {
 	printk(KERN_ALERT, "my open function\n");
@@ -26,8 +33,8 @@ static int my_write(struct file *file, const char __user *user_buffer,
 
 
 /*global variables*/
-struct io_desc cir_que[QUESIZE];
-int que_count = 0;
+struct myio_cir_que myioque; 
+EXPORT_SYMBOL(myioque);
 struct proc_dir_entry *my_proc_dir;
 struct proc_dir_entry *my_proc_file;
 struct file_operations myproc_fops = { .owner = THIS_MODULE,
@@ -38,12 +45,16 @@ struct timespec my_bio_time;
 EXPORT_SYMBOL(my_bio_time);
 
 static int __init init_my_module(void) {
+	/*init circular queue*/
+	myioque.que_count = 0;
+	myioque.fir_index = 0;
+	mtioque.curr_index = 0;
+	
+	/*create proc*/
 	my_proc_dir = proc_mkdir("oslab_dir", NULL);
 	my_proc_file = proc_create("oslab_file", 0600, my_proc_dir, &myproc_fops);
 	
 	printk(KERN_ALERT "module starts.\n");
-	getnstimeofday(&my_bio_time);
-	printk("sec time = %ld, nc sec time = %ld\n", my_bio_time.tv_sec , my_bio_time.tv_nsec);
 	return 0;
 }
 
@@ -51,6 +62,14 @@ static void __exit exit_my_module(void) {
 	printk(KERN_ALERT "module terminates\n");
 	return;
 }
+
+int add_myioque(struct myio_cir_que *que, struct timespec *time, ) {
+	if (que->que_count == QUESIZE)
+		print_que_to_proc(que); /*at here, initatilize que member like count, index ...*/
+
+	/*store data*/
+}
+
 
 module_init(init_my_module);
 module_exit(exit_my_module);
